d6f8106b83998ff45f39b51730626dee63163743
SARL Focus System <83344503+focus-system@users.noreply.github.com>
2021-09-14 18:05:58 +0100

    Add files via upload

---
+++ fs_reference/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'References',
+    'summary':"""This module automatically create customer and supplier references""",
+    'version': '14.0.0.1.0',
+    'author':'SARL FOCUS SYSTEM.',
+    'maintainer': 'SARL FOCUS SYSTEM.',
+    'contributors':['contact <contact@focussystem.dz>'],
+    'website':'http://www.focussystem.dz',
+    'depends': ['contacts'],
+    'data': [
+        'views/contact_views_inherit.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application':True,
+}
