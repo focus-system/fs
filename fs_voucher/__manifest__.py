e317017565485158f7ae4852731c9e167a224f3d
SARL Focus System <83344503+focus-system@users.noreply.github.com>
2021-09-14 18:14:10 +0100

    Add files via upload

---
+++ fs_voucher/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Voucher',
+    'summary':"""This module allows the management of vouchers""",
+    'version': '14.0.0.1.0',
+    'category': 'sale',
+    'author':'SARL FOCUS SYSTEM.',
+    'maintainer': 'SARL FOCUS SYSTEM.',
+    'contributors':['contact <contact@focussystem.dz>'],
+    'website':'http://www.focussystem.dz',
+    'depends': ['account', 'sale'],
+    'data': [
+        'views/sale_views_inherit.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application':True,
+}
